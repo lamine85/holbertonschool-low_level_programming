59514c2aef1cc5d431c91bb18eb0443129973231
lamine85 <8744@holbertonstudents.com>
2024-02-22 13:28:18 +0000

    imprimer

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("                 ,%%%%%%%%, \n");
-    printf("              ,%%/\\%%%%/\\%%, \n");
-    printf("             ,%%%\\c '' J/%%, \n");
-    printf("  %.10s  ,%%%00\\c ' J/00%%, \n", "Your outcome is a piece of art");
-    printf("       ,%%%/00\\ \\''/ 00%%%,\n");
-    printf("      %%%%/0000\\\\'/0000\\%%% \n");
-    printf("      %%%%|0000|0000|%%%%% \n");
-    printf("      %%%%\\0000/\\0000/%%%% \n");
-    printf("       %%%%\\000/''\\000/%%%% \n");
-    printf("        %%%%/00|  |00\\%%%%% \n");
-    printf("         %%%/000|  |000\\%%% \n");
-    printf("          %%/0000|  |0000\\%% \n");
-    printf("           %/00000|  |00000\\% \n");
-    printf("            |/0000(())0000\\| \n");
-    printf("             \\0000` ''0000/ \n");
-    printf("              `\"\"'   '\"\"'\n");
-    return 0;
+/**
+ * main
+ * printf -imprime une phrase
+ * Return: (SUCCES)
+ */
+
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
