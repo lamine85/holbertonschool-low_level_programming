5a0d02ff2ad2edbbbbcc708b11ba2288eb1b120d
lamine85 <8744@holbertonstudents.com>
2024-02-24 21:13:00 +0000

    better

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+
+	srand(time(NULL));
+
+
+	int n = rand();
+	if 2 > 0:	
+sh: 1: q: not found
+
+
+	printf("Nombre aleatoire: %d\n", n);
+
+}
+	return 0;
+
