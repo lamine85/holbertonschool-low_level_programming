f1f4171ee8b392479f120a6acfacc63683511348
lamine85 <8744@holbertonstudents.com>
2024-02-26 15:24:49 +0000

    r

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,22 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+*_isupper -descriptions
+*@c:nombre
+*
+*Return:0
+*/
+
 int _isupper(int c)
 {
-  if (c >= 'A' && c <= 'Z')
-		  {
-      return (1);
-      }
-      else
-      {
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+	{
 	return (0);
-      }
+	}
+}
 
-      } 
-       
-   
-    
