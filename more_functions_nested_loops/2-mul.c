4b0a3ea3af98733ccbdea1eb736f23260674b4be
lamine85 <8744@holbertonstudents.com>
2024-02-27 21:44:08 +0000

    *

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * int mul(int a, int b) - calcule d'une multiplication
+ * @a An input characte for multiplication
+ * @b ''     ''     ''      ''      ''
+ * Return: (SUCCESS)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,2 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
 int _isdigit(int c);
+int mul(int a, int b);
+
+#endif
+
 
