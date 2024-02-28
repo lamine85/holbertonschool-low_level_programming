2abf1e8ef1276c59fa0e65d6efa4594e82fb9ba7
lamine85 <8744@holbertonstudents.com>
2024-02-28 09:59:30 +0000

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ *print_numbers
+ *foctions : boucles
+ */
+void print_numbers(void)
+{
+	for (i=0; i<=9; i++)
+		_putchar(i);	       
+putchar('\n');
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -5,8 +5,13 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int _isdigit(int c);
-int mul(int a, int b);
+/**
+ * fichier de configuration + appel de librairie
+ * avec les fonctions de rappele
+ */
+
+void print_numbers(void);
+int _putchar(char c);
 
 #endif
 
