a4673be398d0b77f94c0e45e57cf6e481d682ef3
lamine85 <8744@holbertonstudents.com>
2024-02-28 15:54:28 +0000

    4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,19 @@
-#iniclude "main.h"
+#include "main.h"
 
 /*
  *Print the numbers from 0 to 9
- *\
+ */
 
- void print_most_numbers(void)
- {
-    int i; 
+void print_most_numbers(void)
+{
+	int i;
 
-   for (i = '0'; i <= '9'; i++);
-      {
-    (if i= '2' && i !='4')
-      { 
-    _putchar(i);
-    }
-{_putchar (*\n');
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar ('\n');
+}
