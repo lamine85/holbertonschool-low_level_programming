4a3ea96be9969ec830f8bc2f18fdbc53551d939f
lamine85 <8744@holbertonstudents.com>
2024-02-29 09:32:56 +0000

    3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
-/*
+/**
  *print_numbers
  *foctions : boucles
  */
+
 void print_numbers(void)
 {
-	int i;
-	for (i=0; i<=9; i++)
-		_putchar(i);	       
+	char i;
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}		
 putchar('\n');
+}
